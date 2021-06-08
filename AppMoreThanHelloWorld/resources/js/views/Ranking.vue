3017805dcb7d34c84a601c7d8b550d8e1f9c7fb2
Leyla <leyla.benkais@heig-vd.ch>
2021-06-08 15:58:40 +0200

    [UPDATE] Ranking page styling

--- AppMoreThanHelloWorld/resources/js/views/Ranking.vue
+++ AppMoreThanHelloWorld/resources/js/views/Ranking.vue
@@ -8,82 +8,175 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="page">
     <h1>Classement</h1>
-    <section>
-        <div id="place2">
-            <p>Nom 2</p>
-        </div>
-      <img id="podium" src="/img/classement_couleur.png" alt="podium" />
+    <section class="container caption">
+      <img
+        class="img-fluid"
+        id="podium"
+        src="/img/classement_couleur.png"
+        alt="podium"
+      />
+      <p class="place1">
+        <span> Place 1 </span> <br />
+        points
+      </p>
+      <p class="place2">
+       <span> Place 2 </span><br />
+        points
+      </p>
+      <p class="place3">
+       <span> Place 3 </span> <br />
+        points
+      </p>
     </section>
-    <section>
-      <div class="container">
-        <p class="place">4</p>
-        <p class="pseudo">Nom</p>
-        <p class="points">256</p>
-        <img class="image" src="/img/edelweiss.png" alt="points" />
+
+    <section class="container">
+      <div class="row">
+        <p class="col-2 place">4</p>
+        <p class="col-6 pseudo">Nom</p>
+        <p class="col-2 points">256</p>
+        <div class="col-2">
+          <img class="image" src="/img/edelweiss.png" alt="points" />
+        </div>
       </div>
-      <div class="container">
-        <p class="place">5</p>
-        <p class="pseudo">Nom</p>
-        <p class="points">256</p>
-        <img class="image" src="/img/edelweiss.png" alt="points" />
+      <div class="row">
+        <p class="col-2 place">5</p>
+        <p class="col-6 pseudo">Nom</p>
+        <p class="col-2 points">256</p>
+        <div class="col-2">
+          <img class="image" src="/img/edelweiss.png" alt="points" />
+        </div>
       </div>
       <div class="espace">
-          . <br>
-          . <br>
-          . <br>
-          .
+        <img id="espace" src="/img/petits_points.png" alt="espace" />
       </div>
-      <div class="container">
-        <p class="place">4</p>
-        <p class="pseudo">Nom</p>
-        <p class="points">256</p>
-        <img class="image" src="/img/edelweiss.png" alt="points" />
-      </div>
+      <span class="row">
+        <p class="col-2 place">6</p>
+        <p class="col-6 pseudo">Nom</p>
+        <p class="col-2 points">256</p>
+        <div class="col-2">
+          <img class="image" src="/img/edelweiss.png" alt="points" />
+        </div>
+      </span>
     </section>
   </div>
 </template>
 
 <style scoped>
 #podium {
-  width: 40%;
+  width: 90vw;
+  max-width: 600px;
   padding-top: 8rem;
   padding-bottom: 3rem;
 }
-.place {
-    grid-area: place;
+
+#espace {
+  width: 1%;
+  min-width: 8px;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
 }
+
 .pseudo {
-    grid-area: pseudo;
-    text-align: left;
+  text-align: left;
+}
+
+.place {
+  text-align: right;
 }
 .points {
-    grid-area: points;
-    text-align: right;
-    padding-right: 5px;
+  text-align: right;
+  padding-right: 5px;
 }
 .image {
-    grid-area: image;
-   width: 40%;
+  width: 30px;
+  display: block;
+  margin-left: 0;
 }
-.container {
-    display: grid;
-    grid-template-columns: 70px auto 80px 70px;
-    grid-template-areas: "place pseudo points image";
-    padding-left: 15rem;
-    padding-right: 15rem;
+
+.place2 {
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-285%, -20%);
+  color:#2D327D;
 }
-/*
-.espace {
-    font-size: 50px;
-    font-weight: bold;
-    color: red;
-}*/
-#place2 {
+.place1 {
+  position: absolute;
+  top: 15%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #DA291C;
+}
+.place3 {
+  position: absolute;
+  top: 33%;
+  left: 50%;
+  transform: translate(182%, 100%);
+  color: #F7DB3E;
+}
+
+.caption {
+  position: relative;
+  margin-bottom: 2%;
+}
+
+span {
+font-weight: bold;
+}
+
+@media (max-width: 576px) {
+
+  .image {
+    width: 20px;
+     text-align: left;
+  }
+
+  .place2 {
     position: absolute;
-    margin-left: 20rem;
-    margin-top: 20rem;
-    
+    top: 30%;
+    left: 45%;
+    transform: translate(-17vw, 1vw);
+    font-size: 15px;
+  }
+  .place1 {
+    position: absolute;
+    top: 23%;
+    left: 52%;
+    transform: translate(0vw, -8vw);
+    font-size: 15px;
+  }
+  .place3 {
+    position: absolute;
+    top: 33%;
+    left: 50%;
+    transform: translate(25vw, 13vw);
+    font-size: 15px;
+  }
+}
+
+@media (max-width: 575.98px) {
+  .place2 {
+    position: absolute;
+    top: 30%;
+    left: 45%;
+    transform: translate(-23vw, 3vw);
+    font-size: 15px;
+  }
+  .place1 {
+    position: absolute;
+    top: 23%;
+    left: 52%;
+    transform: translate(-6vw, -5vw);
+    font-size: 15px;
+  }
+  .place3 {
+    position: absolute;
+    top: 33%;
+    left: 50%;
+    transform: translate(19vw, 15vw);
+    font-size: 15px;
+  }
 }
 </style>
