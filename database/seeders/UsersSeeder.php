4e9a7a080364909779bd4d09d817cc7e32070f57
Larry <larrylam.business@gmail.com>
2021-06-17 15:34:02 +0200

    [UPDATE] Clean user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -18,26 +18,26 @@ class UsersSeeder extends Seeder
 
         $users = [
             array(
-                'admin', 'admin@admin.ch', '123', '7400', 1
+                'admin', 'admin@admin.ch', 'mthw123$', '0', 1
             ),
-            array(
-                'user', 'user@user.ch', '123', '5700', 0
-            ),
-            array(
-                'user1', 'user1@user.ch', '123', '3500', 0
-            ),
-            array(
-                'user2', 'user2@user.ch', '123', '6700', 0
-            ),
-            array(
-                'user3', 'user3@user.ch', '123', '8100', 0
-            ),
-            array(
-                'user4', 'user4@user.ch', '123', '4000', 0
-            ),
-            array(
-                'user5', 'user5@user.ch', '123', '7300', 0
-            ),
+            // array(
+            //     'user', 'user@user.ch', '123', '5700', 0
+            // ),
+            // array(
+            //     'user1', 'user1@user.ch', '123', '3500', 0
+            // ),
+            // array(
+            //     'user2', 'user2@user.ch', '123', '6700', 0
+            // ),
+            // array(
+            //     'user3', 'user3@user.ch', '123', '8100', 0
+            // ),
+            // array(
+            //     'user4', 'user4@user.ch', '123', '4000', 0
+            // ),
+            // array(
+            //     'user5', 'user5@user.ch', '123', '7300', 0
+            // ),
         ];
 
         DB::table('users')->delete();
